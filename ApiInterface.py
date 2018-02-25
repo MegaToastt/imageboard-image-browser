a464d11b8115148510cc39621dc3b78d1273a43b
Mathew Stein <mathew.stein97@gmail.com>
2018-02-26 03:18:42 +1100

    Added more functionality - can select board, thread, and post through GUI. No image is visible yet

--- ApiInterface.py
+++ ApiInterface.py
@@ -22,6 +22,52 @@ class ApiInterface():
                 boardList.append(boardDict)
             self.boardInfo = boardList
         return self.boardInfo
+    
+    def getBoardURL(self, boardName):
+        return 'https://a.4cdn.org/'+ boardName +'/catalog.json'
+
+    def getThreadList(self, boardName):
+        threadList = []
+        # Need to compile all the pages into one list and get required info (name and thread number, etc)
+        threadJSON = self.s.get(self.getBoardURL(boardName)).json()
+        for page in threadJSON:
+            threads = page['threads']
+            for thread in threads:
+                threadDict = {}
+                # Thread number
+                threadDict['no'] = thread['no']
+                # Thread subject (if any)
+                if 'sub' in thread: 
+                    threadDict['sub'] = thread['sub']
+                # Thread comment
+                if 'com' in thread:
+                    threadDict['com'] = thread['com']
+
+                threadList.append(threadDict)
+        return threadList
+
+    def getThreadURL(self, boardName, threadNo):
+        return 'https://a.4cdn.org/'+ boardName +'/thread/'+ str(threadNo) +'.json'
+
+    def getThread(self, boardName, threadNo):
+        postList = []
+        postsJSON = self.s.get(self.getThreadURL(boardName, threadNo)).json()['posts']
+
+        for post in postsJSON:
+            # Skip if no image
+            if 'ext' not in post:
+                continue
+            
+            postDict = {}
+            postDict['no'] = post['no']
+            postDict['filename'] = post['filename']
+            postDict['imageURL'] = 'https://i.4cdn.org/'+ boardName +'/'+ str(threadNo) + post['ext']
+
+            postList.append(postDict)
+        return postList
+
+
+
 
 
 a = ApiInterface()
--- forms/MainWindow.py
+++ forms/MainWindow.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 class MainWindow(QMainWindow):
     def __init__(self, api):
@@ -10,6 +11,8 @@ class MainWindow(QMainWindow):
     def initUI(self):
         self.initMenuBar()
         self.initToolBar()
+        self.initCentralWidget()
+        self.initDock()
 
         self.move(300, 200)
         self.setWindowTitle('Window title here')
@@ -37,14 +40,103 @@ class MainWindow(QMainWindow):
 
         # Create combo box to select board
         boardLabel = QLabel("Board:")
-        boardComboBox = QComboBox(self)
-        boardComboBox.setMaxVisibleItems(20)
+        self.boardComboBox = QComboBox(self)
+        self.boardComboBox.setMaxVisibleItems(20)
         # Get board list and populate the combobox
         boardList = self.api.getBoardList()
         for board in boardList:
-            boardComboBox.addItem(board['board'])
+            self.boardComboBox.addItem(board['board'])
+        # Add "go" button to load the board
+        boardLoadBtn = QPushButton("Go", self)
+        boardLoadBtn.clicked.connect(self.on_boardLoad)
+        boardLoadBtn.setMaximumSize(40,40)
 
 
         # Add widgets, etc to the toolbar
         self.toolbar.addWidget(boardLabel)
-        self.toolbar.addWidget(boardComboBox)
+        self.toolbar.addWidget(self.boardComboBox)
+        self.toolbar.addWidget(boardLoadBtn)
+
+    def initCentralWidget(self):
+        self.placeholderLabel = QLabel("Placeholder")
+        self.setCentralWidget(self.placeholderLabel)
+
+    def initDock(self):
+        ## variable initialization ##
+        self.threadPosts = {}
+        #####
+        sideDock = QDockWidget("Threads", self)
+        sideDock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+        # Removes titlebar on the dock
+        sideDock.setTitleBarWidget(QWidget())
+
+        # Parent widget so it is possible to add 2 widgets inside the dock
+        dockWidget = QWidget()
+        layout = QHBoxLayout()
+
+        self.threadList = QListWidget(sideDock)
+        self.threadList.addItems(("Test1", "Test2", "Test3", "Test4"))
+        #self.threadList.setSizeAdjustPolicy(QListWidget.AdjustToContents)
+
+        self.imageList = QListWidget(sideDock)
+        self.imageList.addItems(("TestImage1", "TestImage2", "TestImage3"))
+
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.setChildrenCollapsible(False)
+        splitter.addWidget(self.threadList)
+        splitter.addWidget(self.imageList)
+
+        layout.addWidget(splitter)
+        #layout.addWidget(self.threadList)
+        #layout.addWidget(self.imageList)
+        dockWidget.setLayout(layout)
+
+        sideDock.setWidget(dockWidget)
+        self.addDockWidget(Qt.LeftDockWidgetArea, sideDock)
+        self.threadList.currentRowChanged.connect(self.on_threadSelect)
+        self.imageList.currentRowChanged.connect(self.on_postSelect)
+
+    def initImageDock(self):
+        sideDock = QDockWidget("Images", self)
+        sideDock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+        self.imageList = QListWidget(sideDock)
+        self.imageList.addItems(("TestImage1", "TestImage2", "TestImage3"))
+        sideDock.setWidget(self.imageList)
+        self.addDockWidget(Qt.LeftDockWidgetArea, sideDock)
+        
+
+    def updateThreadList(self, boardName):
+        self.threads = self.api.getThreadList(boardName)
+        self.threadList.clear()
+
+        for thread in self.threads:
+            if 'sub' in thread:
+                self.threadList.addItem(thread['sub'][:30])
+            elif 'com' in thread:
+                self.threadList.addItem(thread['com'][:30])
+            else:
+                self.threadList.addItem(' ')
+
+    def updatePostList(self, boardName, threadNo):
+        self.imageList.clear()
+        if (boardName, threadNo) not in self.threadPosts:
+            self.threadPosts[(boardName, threadNo)] = self.api.getThread(boardName, threadNo)
+        for post in self.threadPosts[(boardName, threadNo)]:
+            self.imageList.addItem(post['filename'])
+            
+
+    def on_threadSelect(self):
+        current = self.threadList.currentRow()
+        try:
+            self.updatePostList(self.boardComboBox.currentText(), self.threads[current]['no'])
+        except AttributeError:
+            print('att err?')
+
+    def on_postSelect(self):
+        #self.placeholderLabel.setText(self.imageList.currentText())
+        pass
+
+    # Event when the "go" button is clicked when selecting a board
+    def on_boardLoad(self):
+        print(self.boardComboBox.currentText())
+        self.updateThreadList(self.boardComboBox.currentText())
