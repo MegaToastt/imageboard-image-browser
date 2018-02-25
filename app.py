374569d290b18070c7a69e1d1a934a21778136a0
Mathew Stein <mathew.stein97@gmail.com>
2018-02-26 03:41:26 +1100

    Added cache folder creation/clearing on startup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from forms.MainWindow import MainWindow
@@ -10,9 +11,16 @@ class App:
         self.api = ApiInterface()
 
     def run(self):
+        self.initCacheFolder()
         app = QApplication(sys.argv)
         w = MainWindow(self.api)
         sys.exit(app.exec_())
+
+    def initCacheFolder(self):
+        # delete the existing cache if existing, then make a new empty one
+        if os.path.exists('cache'):
+            shutil.rmtree('cache')
+        os.mkdir('cache')
 
 if __name__ == '__main__':
     app = App()
